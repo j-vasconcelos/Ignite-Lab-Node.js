108803ef335522994690e627c27879611e3e1153
j-vasconcelos <dev.joaovasconcelos@gmail.com>
2022-12-14 00:29:03 -0300

    Aula 02 - Domínio, casos de uso e regras de negócio

---
+++ src/application/entities/notification/notification.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from "./content";
+import { Notification } from "./notification";
+
+describe('Notifications', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizada'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+  
+    expect(notification).toBeTruthy();
+  });  
+});
+
